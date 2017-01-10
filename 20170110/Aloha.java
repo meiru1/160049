f67f3661260f6e49164ba4b373c8a245883fc39b
meiru1 <torii_naoto@yse-c.net>
2017-01-10 16:07:28 +0900

    ALOHA done.

---
+++ 20170110/Aloha.java
@@ -0,0 +1,26 @@
+public class Aloha {
+	public static void main(String[] args) {
+		Alohaman am = new Alohaman();
+
+		am.sayAloha(27);
+			}
+		}
+		class Alohaman {
+
+			private String msg;
+			private String msg2;
+
+			public Alohaman(){
+
+				this.msg = "ALOHA!";
+				this.msg2 = "アロハ！";
+			}
+		public void sayAloha() {
+			System.out.println(this.msg);
+		}
+		public void sayAloha(int n) {
+			for (int i =0; i < n; i++) {
+			System.out.println(this.msg);
+		}
+	}
+}
